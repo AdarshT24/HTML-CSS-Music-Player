875745765c6dd23d2e83d508ca70d704af212599
Adarsh <at457645@gmail.com>
2022-06-29 01:40:08 +0530

    initial commit

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [{
+        name: "Thoda Thoda Pyaar",
+        artist: "Stebin Ben, Nilesh Ahuja",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Maine Royaan",
+        artist: "Tanveer Evan",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Aasman Ko Chukar",
+        artist: "Daler Mehndi",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Aaoge Jab Tum",
+        artist: "Ustad Rashid Khan",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Gundagardi",
+        artist: "Himmat Sandhu",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Selfmade",
+        artist: "Sidhu Moose Wala",
+        img: "music-6",
+        src: "music-6"
+    },
+
+];
